1516bec269adf44bf13f0ef2ddafe3a07ab0c937
SalvaWebPr0 <salvawebpro@gmail.com>
2023-11-24 21:26:21 -0600

    re revert

--- src/app/servicios/page.jsx
+++ src/app/servicios/page.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ListProducts from '@/components/products/ListProducts';
 import MainServicesComponent from '@/components/services/MainServicesComponent';
 import PageTransition from '@/components/transitions/PageTransition';
-import { getAllLocalProducts } from '@/helpers';
+import { getProducts } from '@/helpers';
 
 const ServiciosPage = async () => {
-  const data = await getAllLocalProducts();
+  const data = await getProducts();
   const products = data.products;
   return (
     <div>
